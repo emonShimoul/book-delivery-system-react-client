d95daafbe7bf7ca46ac0dfd94528e43e536102e7
emonShimoul <emonshimoul@gmail.com>
2022-07-17 12:16:00 +0600

    change navbar color and added some margin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,9 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
+            <Navbar className='py-3' bg="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand><Link className='text-decoration-none text-dark' to="/">Book Delivery System</Link></Navbar.Brand>
+                    <Navbar.Brand><Link className='text-decoration-none text-light' to="/">Book Delivery System</Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -15,9 +15,9 @@ const Header = () => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <NavLink className='text-decoration-none text-dark px-3' to="/allbooks">All Books</NavLink>
-                        <NavLink className='text-decoration-none text-dark px-3' to="/mypurchase">My Purchase</NavLink>
-                        <NavLink className='text-decoration-none text-dark px-3' to="/allpurchase">All Purchase</NavLink>
+                        <NavLink className='text-decoration-none text-light px-3' to="/allbooks">All Books</NavLink>
+                        <NavLink className='text-decoration-none text-light px-3' to="/mypurchase">My Purchase</NavLink>
+                        <NavLink className='text-decoration-none text-light px-3' to="/allpurchase">All Purchase</NavLink>
                     </Nav>
                     <Form className="d-flex">
                         <Link to="/login"><Button variant="outline-success">Login</Button></Link>
