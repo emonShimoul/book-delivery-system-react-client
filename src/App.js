2e0236e294dbf3fc2f267bfcc678484353811d9d
emonShimoul <emonshimoul@gmail.com>
2022-07-17 11:19:25 +0600

    added navbar on header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2 className='text-danger'>Book Delivery System Project</h2>
+      <Header></Header>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header</h2>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#">Book Delivery System</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">All Books</Nav.Link>
+                        <Nav.Link href="#action2">My Purchase</Nav.Link>
+                        <Nav.Link href="#action3">All Purchase</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Button variant="outline-success">Login</Button>
+                    </Form>
+                    </Navbar.Collapse>
+                </Container>
+                </Navbar>
         </div>
     );
 };
