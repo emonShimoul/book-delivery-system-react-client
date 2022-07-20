e62f2986491f817efca76df906f65a1230a75e01
emonShimoul <emonshimoul@gmail.com>
2022-07-20 11:20:46 +0600

    added private route component

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PrivateRoute = ({
+    redirectPath = '/login',
+    children
+}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivateRoute;
