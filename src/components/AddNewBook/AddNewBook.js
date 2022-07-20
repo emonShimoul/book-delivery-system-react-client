f7dafeec899f0c4b0a9ac06da9760ecf4ece3348
emonShimoul <emonshimoul@gmail.com>
2022-07-20 11:09:29 +0600

    added add new book component

---
+++ src/components/AddNewBook/AddNewBook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddNewBook = () => {
+    return (
+        <div>
+            <h1>This is Add New Book</h1>
+        </div>
+    );
+};
+
+export default AddNewBook;
