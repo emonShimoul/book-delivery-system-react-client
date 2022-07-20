10edea1519b79c1f4884d26cd092ef064355c585
emonShimoul <emonshimoul@gmail.com>
2022-07-20 11:25:32 +0600

    completed private route functionality

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const PrivateRoute = ({
     redirectPath = '/login',
     children
 }) => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user} = useAuth(); 
+    const location = useLocation();
+    if(!user.email) {
+        return <Navigate to={redirectPath} replace state={{from: location}} />;
+    }
+    return children;
 };
 
 export default PrivateRoute;
