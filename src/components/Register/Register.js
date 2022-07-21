8cc7b49412cefd6f3adf53c4f679d93367377a74
emonShimoul <emonshimoul@gmail.com>
2022-07-21 11:21:44 +0600

    slight changes on register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -13,22 +13,22 @@ const Register = () => {
             <Form className='w-50 mx-auto mt-5 text-start p-4 border rounded-3' onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label><h5>Full Name</h5></Form.Label>
-                <Form.Control {...register("fullname")} type="fullname" placeholder="Enter Full Name" />
+                <Form.Control {...register("fullname")} type="fullname" placeholder="Enter Full Name" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label><h5>Email Address</h5></Form.Label>
-                <Form.Control {...register("email")} type="email" placeholder="Enter Email" />
+                <Form.Control {...register("email")} type="email" placeholder="Enter Email" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label><h5>Password</h5></Form.Label>
-                <Form.Control {...register("password")} type="password" placeholder="Enter Password" />
+                <Form.Control {...register("password")} type="password" placeholder="Enter Password" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicContact">
                 <Form.Label><h5>Contact Number</h5></Form.Label>
-                <Form.Control {...register("contactNumber")} type="number" placeholder="Enter Your Phone Number" />
+                <Form.Control {...register("contactNumber")} type="number" placeholder="Enter Your Phone Number" required/>
                 </Form.Group>
 
                 <div className='text-center'>
