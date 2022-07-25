c537808302fe4ad158ac9e7b560050e6d3a1fc80
emonShimoul <emonshimoul@gmail.com>
2022-07-25 12:22:32 +0600

    display purchase component

---
+++ src/components/DisplayPurchase/DisplayPurchase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayPurchase = () => {
+    return (
+        <div>
+            <h2>Display Purchase</h2>
+        </div>
+    );
+};
+
+export default DisplayPurchase;
