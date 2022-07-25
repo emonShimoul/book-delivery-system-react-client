cb82bc552379df3b8755cd4616ae267a2e332b02
emonShimoul <emonshimoul@gmail.com>
2022-07-25 12:35:57 +0600

    display all data from DisplayPurchase component

--- src/components/DisplayPurchase/DisplayPurchase.js
+++ src/components/DisplayPurchase/DisplayPurchase.js
@@ -3,9 +3,17 @@ import React from 'react';
 const DisplayPurchase = (props) => {
     const purchasedBookData = props.purchasedBookData;
     console.log(purchasedBookData);
+    const {name, phone, email, date, address, bookname, bookprice} = purchasedBookData;
     return (
-        <div>
-            <h2>Display Purchase</h2>
+        <div className='bg-secondary text-white m-4 py-4 rounded-3'>
+            <h5>{bookname}</h5>
+            <p>{address}</p>
+            <p>{phone}</p>
+            <p>{date}</p>
+            <p>{name}</p>
+            <p>{email}</p>
+            <p><small>{bookprice}</small></p>
+            {/* <button className='btn btn-warning' onClick={() => handleRemoveUserEvents(userData._id)}>Remove</button> */}
         </div>
     );
 };
