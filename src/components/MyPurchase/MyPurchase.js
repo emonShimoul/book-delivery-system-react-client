208dd8bc6ddabc6aa1c028e9fe374fe1d0c78cc5
emonShimoul <emonshimoul@gmail.com>
2022-07-25 12:09:54 +0600

    fetch data for specific user

--- src/components/MyPurchase/MyPurchase.js
+++ src/components/MyPurchase/MyPurchase.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const MyPurchase = () => {
+    const[myPurchase, setMyPurchase] = useState([]);
+    const {user} = useAuth();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/purchasedBooks/${user.email}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(res => res.json())
+        .then(data => setMyPurchase(data));
+    }, []);
+
+    console.log(myPurchase);
+ 
     return (
         <div>
             <h2>My Purchase</h2>
