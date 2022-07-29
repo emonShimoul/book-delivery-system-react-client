e03475b5359e4d7588e70b7ffaaab87cb406958f
Shimoul Uddin Emon <emonshimoul@gmail.com>
2022-07-29 22:02:04 +0600

    get alpurchased book info

--- src/components/AllPurchase/AllPurchase.js
+++ src/components/AllPurchase/AllPurchase.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const AllPurchase = () => {
+    const[allPurchase, setAllPurchase] = useState([]);
+    const {user} = useAuth();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/purchasedBooks`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(res => res.json())
+        .then(data => setAllPurchase(data));
+    }, []);
+    console.log(allPurchase);
     return (
         <div>
             <h2>All Purchase</h2>
