27685bb370d0ab97c4d5bc64eec3f1f071327307
Shimoul Uddin Emon <emonshimoul@gmail.com>
2022-07-31 23:51:12 +0600

    display email

--- src/components/DisplayPurchase/DisplayPurchase.js
+++ src/components/DisplayPurchase/DisplayPurchase.js
@@ -13,6 +13,7 @@ const DisplayPurchase = (props) => {
             <p>{phone}</p>
             <p>{date}</p>
             <p>{name}</p>
+            <p>{email}</p>
             <p><small>Price: {bookprice}</small></p>
             <button className='btn btn-warning me-1 px-3' onClick={() => handleRemoveMyPurchase(purchasedBookData._id)}>Remove</button>
             <button className='btn btn-dark ms-1 px-4'>Edit</button>
