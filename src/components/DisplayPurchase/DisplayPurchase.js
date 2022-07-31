f83077980a98f3379cb8d67d413c45d13e948770
Shimoul Uddin Emon <emonshimoul@gmail.com>
2022-07-31 23:52:08 +0600

    remove unnecessary comments

--- src/components/DisplayPurchase/DisplayPurchase.js
+++ src/components/DisplayPurchase/DisplayPurchase.js
@@ -3,7 +3,6 @@ import React from 'react';
 const DisplayPurchase = (props) => {
     const purchasedBookData = props.purchasedBookData;
     const handleRemoveMyPurchase = props.handleRemoveMyPurchase;
-    // console.log(purchasedBookData);
     const {name, phone, email, date, address, bookname, bookprice} = purchasedBookData;
 
     return (
@@ -17,7 +16,6 @@ const DisplayPurchase = (props) => {
             <p><small>Price: {bookprice}</small></p>
             <button className='btn btn-warning me-1 px-3' onClick={() => handleRemoveMyPurchase(purchasedBookData._id)}>Remove</button>
             <button className='btn btn-dark ms-1 px-4'>Edit</button>
-            {/* <button className='btn btn-warning' onClick={() => handleRemoveUserEvents(userData._id)}>Remove</button> */}
         </div>
     );
 };
